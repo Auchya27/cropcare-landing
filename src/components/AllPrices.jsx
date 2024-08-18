062a196c93a88722cd8d98a2d32b7b8645eff85d
Your Name <your.email@example.com>
2024-08-18 18:19:33 +0530

    initial commit

---
+++ src/components/AllPrices.jsx
@@ -0,0 +1,191 @@
+import React from 'react'
+import punch from '../assets/punch-car.png'
+import alto from '../assets/alto-blue.jpg'
+import unicorn from '../assets/unicorn.jpg'
+import power from '../assets/power-tiller.webp'
+import shine from '../assets/shine.jpg'
+import weed from '../assets/weed.jpg'
+import pipe from '../assets/lateral-pipe.webp'
+import seeder from '../assets/seeder-machine.webp'
+import water from '../assets/water-purifier.png'
+import cooler from '../assets/air-cooler.webp'
+import tracktor from '../assets/tracktor.webp'
+import powert from '../assets/powert.png'
+import tools from '../assets/farming-tools.png'
+import tv from '../assets/tv.png'
+import smartphone from '../assets/smartphone.png'
+import stp from '../assets/stp-spraypump.jpg'
+import khurpani from '../assets/khurpani-yantra.jpg'
+import ata from '../assets/ata-c.jpg'
+import ranger from '../assets/ranger.jpg'
+import kolpani from '../assets/kolpani-yantra.png'
+import tarpaulin from '../assets/Tarpaulin.jpg'
+import spray from '../assets/spray-pump.jpg'
+
+const AllPrices = () => {
+  return (
+    <div>
+        <div className='m-7'>
+        <h5 className="mb-2 text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-900 text-center">Featured Categories</h5>
+        <p className="font-normal text-gray-700 dark:text-gray-400 text-center text-xl">Visit our shop to see amazing creations from our designers.</p>
+        </div>
+      
+      
+
+<div className="grid grid-cols-2 gap-2 mr-16 ml-16">
+        <div className='transition-transform duration-300 ease-in-out transform hover:scale-105'>
+            <img className="h-auto max-w-full rounded-sm p-7" src={punch} alt="punch" />
+            <p className="text-center text-2xl font-semibold">
+              Tata Punch
+            </p>
+          
+        </div>
+        <div className='transition-transform duration-300 ease-in-out transform hover:scale-105'>
+            <img className="h-auto max-w-full rounded-sm p-7 mt-6" src={alto} alt="alto" />
+            <p className="text-center text-2xl font-semibold mt-12">
+              Maruti Alto
+            </p>
+        </div>
+        <div className='transition-transform duration-300 ease-in-out transform hover:scale-105'>
+            <img className="h-auto max-w-full rounded-sm p-7 ml-20 mt-12" src={unicorn} alt="" />
+            <p className="text-center text-2xl font-semibold">
+              Honda Unicorn
+            </p>
+          
+        </div>
+        <div className='transition-transform duration-300 ease-in-out transform hover:scale-105'>
+            <img className="h-auto max-w-full rounded-sm p-7 ml-20" src={shine} alt="" />
+            <p className="text-center text-2xl font-semibold">
+              Honda Shine 100
+            </p>
+          
+        </div>
+        <div className='transition-transform duration-300 ease-in-out transform hover:scale-105'>
+            <img className="h-auto max-w-full rounded-sm p-7" src={tracktor} alt="" />
+            <p className="text-center text-2xl font-semibold mt-36">
+                Tracktor 45 H.P.
+            </p>
+          
+        </div>
+        <div className='transition-transform duration-300 ease-in-out transform hover:scale-105'>
+            <img className="h-auto max-w-full rounded-sm p-7" src={powert} alt="" />
+            <p className="text-center text-2xl font-semibold">
+              Power Tiller 15 H.P.
+            </p>
+          
+        </div>
+        
+        
+    </div>
+
+
+<div className="grid grid-cols-2 md:grid-cols-4 gap-4 mr-16 ml-16 mt-20 mb-20">
+   
+    <div className='transition-transform duration-300 ease-in-out transform hover:scale-105'>
+        <img className="h-auto max-w-full rounded-sm mt-16" src={power} alt="power-tiller"/>
+        <p className="text-center text-lg font-semibold mt-11">
+              Mini Power Tiller
+        </p>
+    </div>
+   
+    <div className='transition-transform duration-300 ease-in-out transform hover:scale-105'>
+        <img className="h-auto max-w-full rounded-sm" src={weed} alt="weed" />
+        <p className="text-center text-lg font-semibold">
+              Kolapni Yantra
+        </p>
+    </div>
+    <div className='transition-transform duration-300 ease-in-out transform hover:scale-105'>
+        <img className="h-auto max-w-full rounded-sm" src={pipe} alt="" />
+        <p className="text-center text-lg font-semibold">
+            Lateral-3 Bundle
+        </p>
+    </div>
+    <div className='transition-transform duration-300 ease-in-out transform hover:scale-105'>
+        <img className="h-auto max-w-full rounded-sm" src={seeder} alt="" />
+        <p className="text-center text-lg font-semibold">
+              Manual Seeder Machine
+        </p>
+    </div>
+    <div className='transition-transform duration-300 ease-in-out transform hover:scale-105'>
+        <img className="h-auto max-w-full rounded-sm mt-10" src={water} alt="" />
+        <p className="text-center text-lg font-semibold mt-1">
+              Water Purifier
+        </p>
+    </div>
+    <div className='transition-transform duration-300 ease-in-out transform hover:scale-105'>
+        <img className="h-auto max-w-full rounded-sm" src={cooler} alt="" />
+        <p className="text-center text-lg font-semibold">
+              Air Cooler
+        </p>
+    </div>
+
+ 
+    <div className='transition-transform duration-300 ease-in-out transform hover:scale-105'>
+        <img className="h-auto max-w-full rounded-sm" src={tools} alt="" />
+        <p className="text-center text-lg font-semibold mt-10">
+              Farmimg Tools
+        </p>
+    </div>
+    <div className='transition-transform duration-300 ease-in-out transform hover:scale-105'>
+        <img className="h-auto max-w-full rounded-sm mt-5" src={tv} alt="" />
+        <p className="text-center text-lg font-semibold mt-6">
+              T.V
+        </p>
+    </div>
+    <div className='transition-transform duration-300 ease-in-out transform hover:scale-105'>
+        <img className="h-auto max-w-full rounded-sm" src={smartphone} alt="" />
+        <p className="text-center text-lg font-semibold">
+              Smartphone
+        </p>
+    </div>
+    <div className='transition-transform duration-300 ease-in-out transform hover:scale-105'>
+        <img className="h-auto max-w-full rounded-sm mt-4" src={stp} alt="" />
+        <p className="text-center text-lg font-semibold mt-2">
+              S.T.P Spray Pump
+        </p>
+    </div>
+    <div className='transition-transform duration-300 ease-in-out transform hover:scale-105'>
+        <img className="h-auto max-w-full rounded-sm mt-2" src={khurpani} alt="" />
+        <p className="text-center text-lg font-semibold mt-14">
+              Khurapni Yantra
+        </p>
+    </div>
+    <div className='transition-transform duration-300 ease-in-out transform hover:scale-105'>
+        <img className="h-auto max-w-full rounded-sm ml-20" src={ata} alt="" />
+        <p className="text-center text-lg font-semibold">
+              Ata Chaki
+        </p>
+    </div>
+    <div className='transition-transform duration-300 ease-in-out transform hover:scale-105'>
+        <img className="h-auto max-w-full rounded-sm" src={ranger} alt="" />
+        <p className="text-center text-lg font-semibold">
+              Ranger Cycle
+        </p>
+    </div>
+    <div className='transition-transform duration-300 ease-in-out transform hover:scale-105'>
+        <img className="h-auto max-w-full rounded-sm" src={kolpani} alt="" />
+        <p className="text-center text-lg font-semibold">
+              Kolapni Yantra
+        </p>
+    </div>
+    <div className='transition-transform duration-300 ease-in-out transform hover:scale-105'>
+        <img className="h-auto max-w-full rounded-sm" src={tarpaulin} alt="" />
+        <p className="text-center text-lg font-semibold">
+              Tadpatri
+        </p>
+    </div>
+    <div className='transition-transform duration-300 ease-in-out transform hover:scale-105'>
+        <img className="h-auto max-w-full rounded-sm" src={spray} alt="" />
+        <p className="text-center text-lg font-semibold">
+              Spray-Pump
+        </p>
+    </div>
+</div>
+
+
+
+    </div>
+  )
+}
+
+export default AllPrices
